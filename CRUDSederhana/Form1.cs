de5f9bf5da55153864b0109b20fd022913b1830a
thekill022 <gibfathoni@gmail.com>
2025-03-21 14:37:41 +0700

    membuat handler apabila salah satu row atau column diclick, maka data isi data akan menjadi value dari textbox

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -246,6 +246,20 @@ namespace CRUDSederhana
             MessageBox.Show($"Jumlah Kolom : {dgvMahasiswa.ColumnCount}\nJumlah Baris : {dgvMahasiswa.RowCount}", "Debugging DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        /*membuat handler apabila salah satu row atau column diclick, maka data isi data akan menjadi value dari textbox*/
+        private void dgvMahasiswa_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvMahasiswa.Rows[e.RowIndex];
+
+                txtNIM.Text = row.Cells[0].Value?.ToString();
+                txtNama.Text = row.Cells[1].Value?.ToString();
+                txtEmail.Text = row.Cells[2].Value?.ToString();
+                txtTelepon.Text = row.Cells[3].Value?.ToString();
+                txtAlamat.Text = row.Cells[4].Value?.ToString();
+            }
+        }
 
     }
 }
