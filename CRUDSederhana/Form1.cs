ee458d1423d4a3c6b7b7f561ba24b7514801bed9
thekill022 <gibfathoni@gmail.com>
2025-03-21 14:14:26 +0700

    Membuat koneksi ke database OrganisasiMahasiswa

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -7,6 +7,8 @@ namespace CRUDSederhana
     public partial class Form1 : Form
     {
 
+        /*Membuat koneksi ke database OrganisasiMahasiswa*/
+        private String connection = "Data Source=LAPTOP-5DVR7M3S\\GFB_SERVER; Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
 
         public Form1()
         {
